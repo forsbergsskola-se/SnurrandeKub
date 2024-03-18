f83f5a68ec6f8eb57871825f9c7a644b1a4ff6b2
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2024-03-18 15:43:09 +0100

    Solved it....

--- SpinningCube/Assets/Scripts/GravityScript.cs
+++ SpinningCube/Assets/Scripts/GravityScript.cs
@@ -1,22 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Gyroscope = UnityEngine.Gyroscope;
-
 
 public class GravityScript : MonoBehaviour
 {
-    public static GravitySensor current { get; }
     // Start is called before the first frame update
     void Start()
     {
-        InputSystem.EnableDevice(Gyroscope.current);
+        InputSystem.EnableDevice(GravitySensor.current);
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        var gravity = GravitySensor.current.gravity;
+        Physics.gravity = gravity.value;
     }
 }
